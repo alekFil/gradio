a5ad92adf3bcf9b28b77da220b864ee595f87790
Aleksei Filatov <alekfil.ds@gmail.com>
2024-11-12 13:48:09 +0600

    add predict

--- app/inferences/inference_elements.py
+++ app/inferences/inference_elements.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from inferences.models.elements.elements_model import BiLSTMModel
+from torch.nn.utils.rnn import pad_sequence
 
 
 def set_seed(seed):
@@ -63,3 +64,95 @@ class ModelElementsInference:
         predicted_labels = outputs_masked.argmax(dim=-1)
 
         return predicted_labels, predicted_probs
+
+
+# Загрузка модели при старте сервиса
+model_path = "app/inferences/models/elements/checkpoints/checkpoint.pt"
+parameters = (64, 2, 0.3, 198, 0.05, 128, True, True, True, 0.05)
+num_classes = 3
+INFERENCE_ELEMENTS = ModelElementsInference(model_path, parameters, num_classes)
+
+
+def predict(landmarks_data, world_landmarks_data):
+    landmarks_tensor = torch.tensor(landmarks_data)
+    world_landmarks_tensor = torch.tensor(world_landmarks_data)
+    print(f"{landmarks_tensor.shape=}")
+    print(f"{world_landmarks_tensor.shape=}")
+
+    def collate_ml(batch):
+        (
+            lengths,
+            swfeatures,
+            sfeatures,
+        ) = zip(*batch)
+
+        lengths = torch.tensor(lengths).flatten()
+
+        swfeatures = pad_sequence(swfeatures, batch_first=True)
+        swfeatures = swfeatures.view(swfeatures.shape[0], swfeatures.shape[1], -1)
+
+        sfeatures = pad_sequence(sfeatures, batch_first=True)
+        sfeatures = sfeatures.view(sfeatures.shape[0], sfeatures.shape[1], -1)
+
+        features = torch.cat(
+            [
+                swfeatures,
+                sfeatures,
+            ],
+            dim=2,
+        )
+
+        return lengths, features
+
+    # Определим длину каждой последовательности и необходимое количество батчей
+    sequence_length = 25
+    num_sequences = (
+        landmarks_tensor.shape[0] + sequence_length - 1
+    ) // sequence_length  # Округление вверх
+
+    # Разделим данные на последовательности по 25 элементов
+    sequences = []
+
+    for i in range(num_sequences):
+        start_idx = i * sequence_length
+        end_idx = min(start_idx + sequence_length, landmarks_tensor.shape[0])
+
+        # Получаем последовательность и вычисляем её истинную длину
+        seq_landmarks = landmarks_tensor[start_idx:end_idx]
+        seq_world_landmarks = world_landmarks_tensor[start_idx:end_idx]
+        true_length = seq_landmarks.shape[0]
+
+        # Дополняем последовательности нулями до длины 25, если они короче
+        if true_length < sequence_length:
+            padding = torch.zeros(sequence_length - true_length, 33, 3)
+            seq_landmarks = torch.cat([seq_landmarks, padding], dim=0)
+            seq_world_landmarks = torch.cat([seq_world_landmarks, padding], dim=0)
+
+        # Добавляем последовательности и их длины в список
+        sequences.append((true_length, seq_world_landmarks, seq_landmarks))
+
+    lengths_tensor, features = collate_ml(sequences)
+
+    print(f"{features.shape=}")
+    print(f"{lengths_tensor.shape=}")
+    print(f"{lengths_tensor=}")
+
+    print(f"{features[0]=}")
+
+    lengths_batch, swfeatures_batch = lengths_tensor.clone(), features.clone()
+    print(f"{lengths_batch.shape=}")
+    print(f"{swfeatures_batch.shape=}")
+    print(f"{swfeatures_batch[0]=}")
+
+    max_len = lengths_batch.max()
+    mask = torch.arange(max_len).expand(
+        len(lengths_batch), max_len
+    ) < lengths_batch.unsqueeze(1)
+
+    predicted_labels, predicted_probs = INFERENCE_ELEMENTS.predict(
+        features=swfeatures_batch,
+        lengths=lengths_batch,
+        mask=mask,
+    )
+
+    return predicted_labels, predicted_probs
--- app/utils/main_process.py
+++ app/utils/main_process.py
@@ -1,15 +1,9 @@
 import os
 
 import gradio as gr
-from inferences.inference_elements import ModelElementsInference
+from inferences.inference_elements import predict
 from inferences.inference_landmarks import LandmarksProcessor
 from utils import utils as u
-
-# Загрузка модели при старте сервиса
-model_path = "app/inferences/models/elements/checkpoints/checkpoint.pt"
-parameters = (64, 2, 0.3, 198, 0.05, 128, True, True, True, 0.05)
-num_classes = 3
-INFERENCE_ELEMENTS = ModelElementsInference(model_path, parameters, num_classes)
 
 LANDMARK_MODELS = {
     "Lite": "app/inferences/models/landmarkers/pose_landmarker_lite.task",
@@ -30,10 +24,11 @@ def process_video(
     # Проверяем кэш на наличие предсказанных скелетных данных
     landmarks_data, world_landmarks_data = u.load_cached_landmarks(video_hash)
     if landmarks_data is None:
-        # Если данных нет в кэше, запускаем процесс и сохраняем результат
+        # Если данных нет в кэше, запускаем процесс расчета и сохраняем результат
         landmarks_data, world_landmarks_data, figure_masks_data = LandmarksProcessor(
             LANDMARK_MODELS["Lite"],
             video_hash,
+            do_resize=True,
         ).process_video(video_file, step=3)
 
         # Сохраняем landmarks_data в кэш
@@ -42,5 +37,8 @@ def process_video(
         print("Данные landmarks загружены из кэша.")
 
     print(landmarks_data[0], world_landmarks_data[0], sep="\n")
+
+    predicted_labels, _ = predict(landmarks_data, world_landmarks_data)
+    print(f"{predicted_labels[405:420]=}")
 
     return os.path.join("app/output/processed_video_compatible.mp4")
